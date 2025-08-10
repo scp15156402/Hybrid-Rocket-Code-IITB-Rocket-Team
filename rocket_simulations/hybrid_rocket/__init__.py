160a65e88e3331a40a0736ed49728046b6ec41fb
Siddhant Patil <siddhantcpatil@gmail.com>
2025-08-10 07:50:47 +0530

    Minor change

--- rocket_simulations/hybrid_rocket/__init__.py
+++ rocket_simulations/hybrid_rocket/__init__.py
@@ -1,17 +1,16 @@
-# hybrid_rocket/__init__.py
-
 __version__ = "0.1.0"
 
-# Public entrypoints
-from .main   import app
-from .plots  import plot_motor_assembly, plot_nozzle_profile
+# Create a Flask app instance from the factory
+from .hybrid_app import create_app
+app = create_app()
+
+from .plots import plot_motor_assembly, plot_nozzle_profile
 from .export import (
     export_simulation_data,
     compute_structural_metrics,
-    get_summary_dict
+    get_summary_dict,
 )
 
-# Define the public API
 __all__ = [
     "app",
     "plot_motor_assembly",
